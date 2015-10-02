36a591d2cb5529e3ac1babc3ebd06db32e7576d7
Pavel Kilin <pavel@asdf404.net>
2015-10-02 11:58:45 +0600

    Pathogen and Nerdtree

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ cp -f "$DIR/bashrc/bashrc" "$HOME/.bashrc"
 touch "$HOME/.bash_aliases"
 
 cp -f "$DIR/vimrc/vimrc" "$HOME/.vimrc"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
 
 
 echo "Done. Now run:

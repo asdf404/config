d141ada456cca003872541ccb7a98fecbac8204c
Pavel Kilin <pavel@asdf404.net>
2015-10-02 11:49:58 +0600

    Install script moved to root dir

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 DIR=$(dirname $0)
 
-cp -f "$DIR/.bashrc" "$HOME/.bashrc"
+cp -f "$DIR/bashrc/bashrc" "$HOME/.bashrc"
 touch "$HOME/.bash_aliases"
+
+cp -f "$DIR/vimrc/vimrc" "$HOME/.vimrc"
+
 
 echo "Done. Now run:
 	source ~/.bashrc"
 
 exit 0
+
